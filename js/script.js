47443fa1b6893c5b2ab8dfcf0ee14eb1680db734
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-08-15 13:38:10 -0700

    format population

--- js/script.js
+++ js/script.js
@@ -9,26 +9,35 @@ const regionSet = new Set();
 function countryDetails(e) {
   console.log(e.currentTarget);
 }
-
+function numberWithCommas(x) {
+  x = String(x).replace(/,/g, "");
+  return parseFloat(x).toLocaleString();
+}
 for (const countryInfo of data) {
   // console.log(countryInfo);
   countries.insertAdjacentHTML(
     "beforeend",
     ` <div class="country-card">
-          <img src="${countryInfo.flags.svg}" alt="flag_${countryInfo.name.common}" />
+          <img src="${countryInfo.flags.svg}" alt="flag_${
+      countryInfo.name.common
+    }" />
+          <div class="card-desc">
           <h2 class="nunito-sans-800">${countryInfo.name.common}</h2>
           <p class="nunito-sans-600">
-            Population
-            <span class="nunito-sans-300">${countryInfo.population}</span>
+          Population
+          <span class="nunito-sans-300">${numberWithCommas(
+            countryInfo.population
+          )}</span>
           </p>
           <p class="nunito-sans-600">
-            Region:
-            <span class="nunito-sans-300">${countryInfo.region}</span>
+          Region:
+          <span class="nunito-sans-300">${countryInfo.region}</span>
           </p>
           <p class="nunito-sans-600">
-            Capital:
-            <span class="nunito-sans-300">${countryInfo.capital}</span>
+          Capital:
+          <span class="nunito-sans-300">${countryInfo.capital}</span>
           </p>
+          </div>
         </div>`
   );
   countries.lastElementChild.addEventListener("click", countryDetails);
